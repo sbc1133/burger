a981764b58f037576521a671c6bbae6068fa05da
Shivalibhalla <shivalibhalla@gmail.com>
2019-10-08 19:37:21 -0700

    assignment version 1

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Burger');
+INSERT INTO burgers (name) VALUES ('Veggie Slam');
+INSERT INTO burgers (name, sleepy) VALUES ('Burger3', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Burger4', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Burger5', true);
+INSERT INTO burgers (name) VALUES ('Burger6');
